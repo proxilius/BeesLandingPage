389cde6003dcbc0f1f5b367725e08b357afdedd3
Grüll Norbert <gnorbert097@gmail.com>
2021-08-26 18:33:09 +0200

    Fix

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -37,7 +37,7 @@ const MainLayout = (landingPageData) => {
   }, [])
 
   return (
-              <div>
+          <div>
             <Header data={landingPageData.landingPageData.Header} />
             <Features data={landingPageData.landingPageData.Features} />
             <About data={landingPageData.landingPageData.About} />
